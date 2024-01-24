2e08aab93381f46a40820588dca2ecb0421f8fe5
Saul Fraire Alvarado <saul.fraire@ensitech.com>
2024-01-23 21:37:34 -0600

    Patron reporitorio

--- SistemaInventario/Program.cs
+++ SistemaInventario/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddScoped<UnidadTrabajo, UnidadTrabajo>();
 
 var app = builder.Build();
 
